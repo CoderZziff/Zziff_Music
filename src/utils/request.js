656c00414dcf48e60e52769c25103b81c86360af
CoderZziff <cnzziff@163.com>
2020-10-12 14:30:44 +0800

    设置不同的生产环境

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,14 +7,7 @@ import store from '../store'
 import Message from 'ant-design-vue/es/message'
 import Toast from 'components/Toast/toast'
 
-// 环境的切换
-if (process.env.NODE_ENV == "development") {
-  axios.defaults.baseURL = "/api";
-} else if (process.env.NODE_ENV == "debug") {
-  axios.defaults.baseURL = "/api";
-} else if (process.env.NODE_ENV == "production") {
-  axios.defaults.baseURL = "/api";
-}
+axios.defaults.baseURL = process.env.VUE_APP_URL;
 
 // 请求超时时间
 axios.defaults.timeout = 30000;
@@ -26,10 +19,6 @@ axios.defaults.headers.post["Content-Type"] =
 // 请求拦截器
 axios.interceptors.request.use(
   config => {
-    // 每次发送请求之前判断是否存在token，如果存在，则统一在http请求的header都加上token，不用每次请求都手动添加了
-    // 即使本地存在token，也有可能token是过期的，所以在响应拦截器中要对返回状态进行判断
-    //  const token = store.state.token;
-    //  token && (config.headers.Authorization = token);
     return config;
   },
   error => {
